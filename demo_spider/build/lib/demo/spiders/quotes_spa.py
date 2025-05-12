91e5a3ad58d8f3bdfcb8518f9937cff6a00747c8
Robert Solovyev <rsolovye@gmail.com>
2025-05-12 17:21:31 +0200

    Enhance test_balancer.sh with authentication parameters and update QuotesSpaSpider with an additional import
    - Added "username" and "password" fields to the JSON configuration in test_balancer.sh for authentication.
    - Changed "auth_enabled" to a string type for consistency.
    - Included an empty line in quotes_spa.py for better readability.

--- demo_spider/build/lib/demo/spiders/quotes_spa.py
+++ demo_spider/build/lib/demo/spiders/quotes_spa.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
 class QuotesSpaSpider(scrapy.Spider):
     name = "quotes_spa"
     start_urls = ["https://quotes.toscrape.com/js/"]
